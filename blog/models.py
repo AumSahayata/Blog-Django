f9b25d1c231e8284c3c1c75e6e575fa789fa62a1
AumSahayata <aumsahayata9@gmail.com>
2023-08-24 17:22:15 +0530

    Database display changes

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,14 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     username = models.ForeignKey(User,related_name="poster",on_delete=models.CASCADE,default=None)
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     username = models.ForeignKey(User,related_name="commentor",on_delete=models.CASCADE,default=None)
     comment = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='c_post',default=None)
+    
+    def __str__(self):
+        return self.comment
